4f134c33bc6c9c87cfbef6945c737b77e08e1890
kharioki <antolisah@gmail.com>
2020-05-08 14:54:54 +0300

    delete customer endpoint

--- routes/customers.js
+++ routes/customers.js
@@ -73,4 +73,19 @@ module.exports = server => {
       );
     }
   });
+
+  // Delete Customer
+  server.del('/customers/:id', async (req, res, next) => {
+    try {
+      const customer = await Customer.findOneAndRemove({ _id: req.params.id });
+      res.send(204);
+      next();
+    } catch (err) {
+      return next(
+        new errors.ResourceNotFoundError(
+          `There is no customer with the id of ${req.params.id}`
+        )
+      );
+    }
+  });
 };

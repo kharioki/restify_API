1ed49fbfb6a7fd7e261071ce8bd63337a37982da
kharioki <antolisah@gmail.com>
2020-05-09 23:22:25 +0300

    user model

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+mongoose.model('User', UserSchema);

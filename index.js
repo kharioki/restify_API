912fed8037aaf813afc700f18a94ad04e31e5595
kharioki <antolisah@gmail.com>
2020-05-10 00:22:41 +0300

    protect routes with auth

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const restify = require('restify');
 const mongoose = require('mongoose');
+const rjwt = require('restify-jwt-community');
+
 const config = require('./config');
 
 const server = restify.createServer();
 
 // Middleware
 server.use(restify.plugins.bodyParser());
+
+// protect routes
+server.use(rjwt({ secret: config.JWT_SECRET }).unless({ path: ['/auth'] }));
 
 server.listen(config.PORT, () => {
   mongoose.set('useFindAndModify', false);
